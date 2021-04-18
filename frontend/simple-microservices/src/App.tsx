bbf955bd97eadfb34ff0654e21dac49075ee9b44
Zachary Faust <zacharyfaust@Zacharys-MacBook-Pro.local>
2021-04-18 11:20:40 -0500

    add image to main page

--- frontend/simple-microservices/src/App.tsx
+++ frontend/simple-microservices/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Card, Paper} from '@material-ui/core';
-import logo from './logo.svg';
+import {Card, CardContent, CardHeader, Paper} from '@material-ui/core';
+import mainImage from './16_9x1080.png';
 import { ClockStream } from './features/clock-stream/ClockStream';
 import {ClockTimer} from './features/clock-timer/ClockTimer';
 import { Header } from './features/header/Header';
@@ -15,16 +15,26 @@ function App() {
     <div className="App">
         <Header></Header>
         <body>
-            <img src={logo} className="App-logo" alt="logo" />
+          <div className="Row">
+            <img src={mainImage} className="App-logo" alt="logo" />
+            <Card>
+              <CardHeader>Microservices Simplified</CardHeader>
+              <CardContent> Paragraph</CardContent>
+            </Card>
+          </div>
             <div className="ButtonColumn">
               <Card className="Card" elevation={5}>
                 <ClockStream></ClockStream>
                 <ClockTimer></ClockTimer>
+                <CardContent> These items use grpc streams to retrieve data.</CardContent>
               </Card>
             </div><br></br>
             <div className="ButtonColumn">
-              <Card elevation={5}>
+              <Card elevation={5} className="sidewaysCard">
                 <AddForm></AddForm>
+                <DivideForm></DivideForm>
+                <AverageForm></AverageForm>
+                <CardContent>These items use http requests to retrieve data.</CardContent>
               </Card>
               <Card elevation={5}>
                 <DivideForm></DivideForm>
